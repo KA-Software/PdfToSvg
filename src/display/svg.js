317f6f5b5f97d69304a21e00a964b1c19fd22526
pramodhkp <prmdh1@gmail.com>
2014-05-21 01:47:07 +0530

    Added some changes

--- src/display/svg.js
+++ src/display/svg.js
@@ -172,7 +172,16 @@ var SVGGraphics = (function SVGGraphicsClosure(ctx) {
 		executeOperatorList: function SVGGraphics_executeOperatorList(operatorList) {
 			var argsArray = operatorList.argsArray;
 			var fnArray = operatorList.fnArray;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 		executeOperatorList: function SVGGraphics_executeOperatorList(
 														operatorList, executionStartIdx,
 											 			continueCallback) {
@@ -180,7 +189,16 @@ var SVGGraphics = (function SVGGraphicsClosure(ctx) {
 			var argsArray = operatorList.argsArray;
 			var fnArray = operatorList.fnArray;
 			var i = executionStartIdx || 0;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 >>>>>>> ac43f77cecf5b4159915cbabac151c3a87db86f7
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 			var argsArrayLen = argsArray.length;
 			console.log(argsArray);
 			console.log(fnArray);
