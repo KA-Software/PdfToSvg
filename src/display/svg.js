ad273ec393fe8b5239f02da8e182a58f8ac12c0f
pramodhkp <prmdh1@gmail.com>
2014-06-24 01:37:31 +0530

    Combine re element into constructPath

--- src/display/svg.js
+++ src/display/svg.js
@@ -533,7 +533,6 @@ var SVGGraphics = (function SVGGraphicsClosure(ctx) {
             y = args[j++];
             var width = args[j++];
             var height = args[j++];
-<<<<<<< HEAD
             var xw = x + width;
             var yh = y + height;
             d += 'M' + x + ' ' + y + 'L' + xw + ' ' + y +
